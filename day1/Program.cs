a5789888d3ef475ffec1a100d70a0e9e34230a3e
He-Zhansheng <yamdister@outlook.com>
2026-08-05 09:38:20 +0800

    添加项目文件。

---
+++ day1/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入你要看的作业题号：");
+            string num = Console.ReadLine();
+            switch (num)
+            {
+                case "1":
+                    {
+                        Console.Write("请输入你要计算的第一个数字：");
+                        decimal numOne = decimal.Parse(Console.ReadLine());
+                        Console.Write("请输入你要计算的第二个数字：");
+                        decimal numTwo = decimal.Parse(Console.ReadLine());
+                        Console.WriteLine($"{numOne}+{numTwo}={numOne + numTwo}");
+                        break;
+                    }
+                case "2":
+                    {
+                        Console.WriteLine("题目描述：");
+                        Console.WriteLine("小明要到美国旅游，可是那里的温度是以华氏度为单位记录的。他需要一个程序将华氏温度（80度）转换为摄氏度，并以华氏度和摄氏度为单位分别显示该温度（提示：摄氏度与芈氏度的转换公式为：摄氏度 = 5/9.0*(华氏度-32)保留3位小数）");
+                        Console.WriteLine("请输入您要转换的数字：");
+                        decimal numOne = decimal.Parse(Console.ReadLine());
+                        Console.WriteLine($"华氏温度：{numOne}摄氏温度：{5 / 9.0m * (numOne - 32):F3}");
+                        break;
+                    }
+                case "3":
+                    {
+                        int numOne = 20;
+                        int numTwo = 10;
+                        Console.WriteLine($"交换前整数1的值为：{numOne}，整数2的值为：{numTwo}");
+                        int numTmp = numOne - numTwo;
+                        numOne -= numTmp;
+                        numTwo += numTmp;
+                        Console.WriteLine($"交换后整数1的值为：{numOne}，整数2的值为：{numTwo}");
+                        break;
+                    }
+                case "4":
+                    {
+                        Console.WriteLine("题目描述： 为抵抗洪水，战士连续作战89小时，编程计算共多少天零多少小时？");
+                        int numDay = 89 / 24;
+                        int numHour = 89 % 24;
+                        Console.WriteLine($"共计{numDay}天{numHour}小时。");
+                        break;
+                    }
+                default: Console.WriteLine("输入的题号有误！"); break;
+            }
+        }
+    }
+}
---
+++ day2/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //字典
+            var scores = new Dictionary<string, int>
+            {
+                { "张三", 85 },
+                { "李四", 92 },
+                { "王五", 78 }
+            };
+            //scores.Add("赵六", 90); // 添加新的键值对
+            //scores.TryAdd("赵六", 90); // 尝试添加新的键值对，如果键已存在则不会添加
+            //scores.Remove("王五"); // 删除指定键的键值对
+            //scores.TryGetValue("李四", out int score); // 尝试获取指定键的值，如果键不存在则返回false
+            //scores.ContainsKey("张三"); // 判断字典中是否包含指定键
+            //scores.ContainsValue(85); // 判断字典中是否包含指定值
+            //scores.Clear(); // 清空字典
+            //scores.Take(scores.Count / 2); // 获取字典中前一半的键值对,返回一个新的字典
+            //scores.Skip(scores.Count / 2); // 获取字典中后一半的键值对,返回一个新的字典,底层是跳过前一半的键值对。
+            //scores.OrderBy(x => x.Value); // 按照值升序排序字典,返回一个新的字典
+            //scores.OrderByDescending(x => x.Value); // 按照值降序排序字典,返回一个新的字典
+
+            //list集合
+            List<int> listTest = new List<int>() { 1, 2, 3, 4, 5 };
+
+            //listTest.Add(1);                                        // 追加数据，语法：list数据.Add(数据)
+            //listTest.AddRange(new List<int>() { 6, 7, 8 });         // 追加集合，语法：list数据.AddRange(集合数据)
+            //listTest.Remove(1);                                     // 删除指定数据，语法：list数据.Remove(数据)
+            //listTest.RemoveAt(listTest.Count - 1);                  // 删除指定索引的数据，语法：list数据.RemoveAt(索引)
+            //listTest.RemoveAll(x => x == 2);                        // 删除所有符合条件的数据，语法：list数据.RemoveAll(条件)
+            //listTest.Clear();                                       // 清空集合，语法：list数据.Clear()
+            //listTest.RemoveRange(0, 2);                             // 删除指定范围的数据，语法：list数据.RemoveRange(起始索引, 删除数量)
+            //listTest.Insert(0, 1);                                    // 在指定索引插入数据，语法：list数据.Insert(索引, 数据)
+            //listTest.InsertRange(0, new List<int>() { 1, 2, 3 });     // 在指定索引插入集合，语法：list数据.InsertRange(索引, 集合数据)
+            //listTest.Contains(1);                                    // 判断集合中是否包含指定数据，语法：list数据.Contains(数据)
+            //listTest.IndexOf(1);                                     // 获取指定数据第一次出现在集合中的索引，语法：list数据.IndexOf(数据)
+            //listTest.LastIndexOf(1);                                 // 获取指定数据最后一次出现在集合中的索引，语法：list数据.LastIndexOf(数据)
+            //listTest.Sort();                                         // 对集合进行升序排序，语法：list数据.Sort()
+            //listTest.Reverse();                                      // 对集合进行反转，语法：list数据.Reverse()
+        }
+    }
+}
